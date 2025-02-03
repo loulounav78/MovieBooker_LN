e22fbd443ec1325eb9e7f198030972c588a04a53
Loris <loulounav78@gmail.com>
2025-02-03 14:39:00 +0100

    end

--- exo/arrayFilter.js
+++ exo/arrayFilter.js
@@ -28,6 +28,30 @@ const users = [
 
 //source https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
-  function tableFilter(table, filtrage) {
-    
-  }
+function tableFilter(table, key, ValeurCible, mode) {
+    switch (mode) {
+        case 'searchApprox':
+            return table.filter(item => 
+                typeof item[key] === 'string' && 
+                item[key].toLowerCase().includes(ValeurCible.toLowerCase())
+            );
+        case 'valueUnder':
+            return table.filter(item => typeof item[key] === 'number' && item[key] < ValeurCible);
+        case 'valueOver':
+            return table.filter(item => typeof item[key] === 'number' && item[key] > ValeurCible);
+        default:
+            throw new Error('mode error : searchApprox, valueUnder, valueOver');
+    }
+}
+console.log("- - - - - - - - - - - - - - - - -");
+console.log("tableFilter(users, name, Alice Dupont, searchApprox)");
+console.log(tableFilter(users, "name", "Alice Dupont", "searchApprox"));
+console.log("- - - - - - - - - - - - - - - - -");
+console.log("tableFilter(users, city, Lyon, searchApprox)");
+console.log(tableFilter(users, "city", "Lyon", "searchApprox"));
+console.log("- - - - - - - - - - - - - - - - -");
+console.log("tableFilter(users, age, 30, valueUnder)");
+console.log(tableFilter(users, "age", 30, "valueUnder"));
+console.log("- - - - - - - - - - - - - - - - -");
+console.log("tableFilter(users, age, 25, valueOver)");
+console.log(tableFilter(users, "age", 25, "valueOver"));
